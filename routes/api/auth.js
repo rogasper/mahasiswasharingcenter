96141de7b3f82d499728537fa65fe413c2125b00
rogasper <royangagas@gmail.com>
2020-08-28 13:52:51 +0700

    login user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 
@@ -13,6 +17,56 @@ router.get('/', auth, async(req, res) => {
     } catch (err) {
         console.error(err.message);
         res.status(400).send('server error');
+    }
+});
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post('/', [
+    check('email', 'Email tidak sesuai').isEmail(),
+    check('password', 'Password tidak sesuai').exists()
+], async(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        // See if user exists
+        if (!user) {
+            return res.status(400).json({ errors: [{ msg: 'pengguna tidak ada' }] });
+        }
+
+        // compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ errors: [{ msg: 'email atau password salah' }] });
+        }
+
+        // Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'), { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            })
+
+        // res.send('User Registered');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error');
     }
 });
 
